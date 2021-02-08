3217487b6e375e596e2b72b194531b05f5d877d9
2gacem7 <gacemcode@gmail.com>
2021-02-08 17:18:21 +0100

    input product

---
+++ shop/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<Product />
+</template>
+
+<script>
+import Product from './components/Product.vue'
+
+export default {
+  name: 'LayoutDefault',
+
+  components: {
+    Product
+  },
+
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ shop/src/components/Product.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="row">
+        <div class="q-pa-md">
+            <div class="col-md-6 col-xs-12">
+                <q-input q-model="myName" type="text" hint="Name" />
+                <q-input q-model="myPrice" type="number" hint="price" />
+                <q-input q-model="myDescription" filled clearable type="textarea" :shadow-text="textareaShadowText" 
+                    @keydown="processTextareaFill" @focus="processTextareaFill"/>
+                <q-input q-model="myImg" @input="val => { file = val[0] }" filled type="file" hint="jpg, jpeg, png"/>
+                <q-btn @click.prevent="onSubmit()" class="q-mt-4" color="black" label="Add" />
+            </div>
+        </div>
+    </div>
+</template>
+ 
+
+<script>
+export default {
+    props: ['product'],
+    methods: {
+        onSubmit(){
+            this.$emit('submit', this.product)
+        }
+    }
+   
+    
+};
+</script>
