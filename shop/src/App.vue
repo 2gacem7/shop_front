9fbc8e0d2eca928b13382be8363899d26c942941
2gacem7 <gacemcode@gmail.com>
2021-02-09 12:44:37 +0100

    shop part

--- shop/src/App.vue
+++ shop/src/App.vue
@@ -1,15 +1,20 @@
 <template>
+<div>
 <Product />
+<Shop />
+</div>
 </template>
 
 <script>
 import Product from './components/Product.vue'
+import Shop from './components/Shop.vue'
 
 export default {
   name: 'LayoutDefault',
 
   components: {
-    Product
+    Product,
+    Shop
   },
 
   data () {
---
+++ shop/src/components/Shop.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div v-for="card in product" :key="card">
+         <p> {{ card.myName }} coucou</p>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+
+
+export default {
+    data () {
+        return {
+            product: this.$store.state.cards
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
